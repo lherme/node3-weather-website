347b994288e546cc352c087c8afb3b2ee281a2b9
Guilherme Nomelini <guilhermenomelini@Guilhermes-MacBook-Pro.local>
2020-12-28 14:47:54 -0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast = require('./utils/forecast.js')
 
 
 const app = express()
+const port = process.env.PORT || 3000  //Using Enviroment's port (Heroku) or local (port: 3000)
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -100,8 +101,8 @@ app.get('*', (req, res) => { //Error Page needs to come last
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 // app.com
 // app.com/help
